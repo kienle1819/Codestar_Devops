2de963327c6cc3d81b786ead2a668fcf864294f1
Kien Le <tonykien2012@gmail.com>
2021-12-09 11:12:10 +0700

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update  -y
+sudo apt install -y software-properties-common mariadb-server mariadb-client
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+password=somepassword
+sudo mysql -e "CREATE DATABASE test;"
+sudo mysql -e "GRANT ALL PRIVILEGES ON test.* TO 'user1'@'%' IDENTIFIED BY '$password';"
+sudo mysql -e "flush privileges;"
